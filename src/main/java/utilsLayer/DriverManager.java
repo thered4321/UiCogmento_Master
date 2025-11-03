ec85581724ed8ecb2a09c0db54c48fc80906ef87
Shubham <shindeshubham8181@gmail.com>
2025-11-04 00:31:59 +0530

    code added to driver manager

--- src/main/java/utilsLayer/DriverManager.java
+++ src/main/java/utilsLayer/DriverManager.java
@@ -1,10 +1,14 @@
 package utilsLayer;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriverManager {
 
+	ThreadLocal<WebDriver> ts= new ThreadLocal<WebDriver>();
+	
 	private DriverManager()
 	{
 		
 	}
-	
+		
 }
